6482168d3fb163c817b5e6d7516e09df87d352cc
zach <zachary.schwerdtfeger@vtsd.com>
2022-10-11 10:24:20 -0400

    sure

--- Lab04bvst.java
+++ Lab04bvst.java
@@ -10,8 +10,7 @@ import java.applet.*;
 public class Lab04bvst extends Applet
 {
 
-    public void paint(Graphics g)
-    {
+    public void paint(Graphics g) {
         // DRAW CUBE
 
 
@@ -19,19 +18,23 @@ public class Lab04bvst extends Applet
         // DRAW SPHERE
 
 
-
         // DRAW INSCRIBED/CIRCUMSCRIBED TRIANGLE
-
 
 
         // DRAW APCS
 
 
-
-        // DRAW PACMEN FLOWER
-
-
-
+        // DRAW PACMEN FLower
+    //cube
+        g.drawLine(100,150,100,400);
+        g.drawLine(200,225,200,475);
+        g.drawLine(100,400,200,475);
+        g.drawLine(100,150,200,225);
+        g.drawLine(200,225,400,225);
+        g.drawLine(200,475,400,475);
+        g.drawLine(400,225,400,475);
+        g.drawLine(400,475,300,400);
+        g.drawLine(300,400,100,400);
     }
 
 }
