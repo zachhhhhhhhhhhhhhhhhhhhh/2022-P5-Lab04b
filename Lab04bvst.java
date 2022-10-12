6b8b290140072d5192294cf8da01966d2514761a
zach <zachary.schwerdtfeger@vtsd.com>
2022-10-12 09:42:07 -0400

    sure

--- Lab04bvst.java
+++ Lab04bvst.java
@@ -35,6 +35,18 @@ public class Lab04bvst extends Applet
         g.drawLine(400,225,400,475);
         g.drawLine(400,475,300,400);
         g.drawLine(300,400,100,400);
+        g.drawLine(300,150,300,400);
+        g.drawLine(300,150, 400, 225);
+        g.drawLine(100,150, 300, 150);
+
+        g.fillRect(175,525,25,125);
+        g.fillRect(225,525,25,125);
+        g.fillRect(200,525, 25,25);
+        g.fillRect(200,575,25,25);
+        g.fillRect(275,525,25,125);
+        g.fillRect(275,525,75,25);
+        g.fillRect(325,525,25,75);
+        g.fillRect(275,575,75,25);
     }
 
 }
